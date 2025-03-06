85e5f67cbfcfe543ec99801d2e755313fd8df91a
Rodrigo <ergcengenharia@gmail.com>
2025-03-05 22:03:47 -0300

    primeiro commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert ('Boas Vindas ao Jogo dos Números Secretos');
+let numeroSecreto =29;
+let chute =prompt('Escolha um número entre 1 e 30');
+if (chute == numeroSecreto) {
+    console.log("Isso aí! você descobriu o número secreto (29)");
+}
