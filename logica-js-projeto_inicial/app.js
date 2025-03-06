5773b8117b98d3ab3279d654a6c8ec4d050596d7
Rodrigo <ergcengenharia@gmail.com>
2025-03-05 23:44:19 -0300

    alterando numero secreto de 29 para 25

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,6 @@
 alert ('Boas Vindas ao Jogo dos Números Secretos');
-let numeroSecreto =26;
+let numeroSecreto =25;
 let chute =prompt('Escolha um número entre 1 e 30');
 if (chute == numeroSecreto) {
-    console.log("Isso aí! você descobriu o número secreto (29)");
+    console.log("Isso aí! você descobriu o número secreto (25)");
 }
